e3ce0daf20e499e9b218552ba6eefea94b88d2b6
Administrator <yangfanqikang@163.com>
2019-12-27 11:47:47 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'ZZR 组件',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: 'https://google.com'},
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        collapsable: false,
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+    ]
+  }
+}
